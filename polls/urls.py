9732d995feb3473d821599973c3ca51964ec737d
Amelia He <amelia.he98@gmail.com>
2019-05-13 18:02:53 -0700

    added test suit

--- polls/urls.py
+++ polls/urls.py
@@ -5,11 +5,11 @@ from . import views
 app_name = 'polls'
 urlpatterns = [
     # exL /polls/
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
     # exL /polls/5/
-    path('specifics/<int:question_id>/', views.detail, name='detail'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     # exL /polls/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     # exL /polls/5/vote/
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
